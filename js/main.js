8d8d3dacbf7bb30f2a03ddd961d6d88dcbadb477
Victor Alves <victoralvssalcantara@gmail.com>
2022-11-25 13:15:11 -0300

    Finalização da web page

--- js/main.js
+++ js/main.js
@@ -1,5 +1,64 @@
 var swiper = new Swiper(".slide-characters", {
     slidesPerView: 3.5,
     spaceBetween: 19,
-    freeMode: true,
-  });
+    freeMode: true, 
+    breakpoints: {
+
+      1440: {
+        slidesPerView: 3.3,
+        spaceBetween: 19
+      },
+    
+      950: {
+        slidesPerView: 2.8,
+        spaceBetween: 19
+      },
+
+      768: {
+        slidesPerView: 1.9,
+        spaceBetween: 19
+      },
+
+      320: {
+        slidesPerView: 1.1,
+        spaceBetween: 10,
+    }
+  }
+});
+
+
+var swiper_features = new Swiper(".features", {
+  slidesPerView: 4.5,
+  spaceBetween: 10,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+
+    1200: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+
+    600: {
+      slidesPerView: 3,
+      spaceBetween: 10
+    },
+
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 30,
+    }
+  },
+
+});
+
+
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
